dea96b5762986916effec3b3b3491551f0a88c80
Haaragard <haaragard@gmail.com>
2023-10-04 22:03:57 -0300

    test: add Order Test

---
+++ tests/Unit/Entity/OrderTest.php
@@ -0,0 +1,99 @@
+<?php declare(strict_types=1);
+
+namespace Tests\Unit\Entity;
+
+use App\Entity\Order;
+use App\Entity\OrderItem;
+use PHPUnit\Framework\TestCase;
+
+class OrderTest extends TestCase
+{
+    public function test_should_throw_error_when_id_is_empty(): void
+    {
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage('Id is required');
+
+        // Arrange
+
+        $orderItem1 = new OrderItem(
+            id: '1',
+            name: 'Order Item 1',
+            price: 100
+        );
+
+
+        // Act
+        new Order(
+            '',
+            '123',
+            [$orderItem1],
+        );
+
+        // Assert
+    }
+
+    public function test_should_throw_error_when_customer_id_is_empty(): void
+    {
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage('Customer Id is required');
+
+        // Arrange
+        $orderItem1 = new OrderItem(
+            id: '1',
+            name: 'Order Item 1',
+            price: 100
+        );
+
+        // Act
+        new Order(
+            '1',
+            '',
+            [$orderItem1],
+        );
+
+        // Assert
+    }
+
+    public function test_should_throw_error_when_order_items_are_empty(): void
+    {
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage('Items are required');
+
+        // Arrange
+
+        // Act
+        new Order(
+            '1',
+            '123',
+            [],
+        );
+
+        // Assert
+    }
+
+    public function test_should_calculate_total(): void
+    {
+        // Arrange
+        $orderItem1 = new OrderItem(
+            id: '1',
+            name: 'Order Item 1',
+            price: 100
+        );
+
+        $orderItem2 = new OrderItem(
+            id: '2',
+            name: 'Order Item 2',
+            price: 200
+        );
+
+        // Act
+        $order = new Order(
+            '1',
+            '123',
+            [$orderItem1, $orderItem2],
+        );
+
+        // Assert
+        $this->assertEquals(300, $order->total());
+    }
+}
