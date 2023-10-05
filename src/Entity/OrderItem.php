399359fff8e7dad309806fd4eea6dae6245df843
Haaragard <haaragard@gmail.com>
2023-10-04 22:27:35 -0300

    feat: add quantity and new methods on order item

--- src/Entity/OrderItem.php
+++ src/Entity/OrderItem.php
@@ -7,11 +7,22 @@ class OrderItem
     public function __construct(
         private string $id,
         private string $name,
+        private int $quantity,
         private int $price
     ) {}
+
+    public function getQuantity(): int
+    {
+        return $this->quantity;
+    }
 
     public function getPrice(): int
     {
         return $this->price;
+    }
+
+    public function getTotalPrice(): int
+    {
+        return $this->getQuantity() * $this->getPrice();
     }
 }
