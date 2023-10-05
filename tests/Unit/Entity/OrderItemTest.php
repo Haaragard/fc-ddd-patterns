fcf30b02a8dafbfd0a499e2c0ff23a70fec9ec32
Haaragard <haaragard@gmail.com>
2023-10-04 22:54:30 -0300

    test: update order item unit tests

--- tests/Unit/Entity/OrderItemTest.php
+++ tests/Unit/Entity/OrderItemTest.php
@@ -11,6 +11,9 @@ class OrderItemTest extends TestCase
         $this->expectException(\Exception::class);
         $this->expectExceptionMessage('Id is required');
 
+        // Arrange
+
+        // Act
         new OrderItem(
             id: '',
             productId: 'p1',
@@ -18,6 +21,8 @@ class OrderItemTest extends TestCase
             quantity: 1,
             price: 100
         );
+
+        // Assert
     }
 
     public function test_should_throw_error_if_product_id_is_empty(): void
@@ -25,6 +30,9 @@ class OrderItemTest extends TestCase
         $this->expectException(\Exception::class);
         $this->expectExceptionMessage('Product Id is required');
 
+        // Arrange
+
+        // Act
         new OrderItem(
             id: '1',
             productId: '',
@@ -32,6 +40,8 @@ class OrderItemTest extends TestCase
             quantity: 1,
             price: 100
         );
+
+        // Assert
     }
 
     public function test_should_throw_error_if_name_id_is_empty(): void
@@ -39,6 +49,9 @@ class OrderItemTest extends TestCase
         $this->expectException(\Exception::class);
         $this->expectExceptionMessage('Name is required');
 
+        // Arrange
+
+        // Act
         new OrderItem(
             id: '1',
             productId: 'p1',
@@ -46,6 +59,8 @@ class OrderItemTest extends TestCase
             quantity: 1,
             price: 100
         );
+
+        // Assert
     }
 
     public function test_should_throw_error_if_quantity_is_less_than_zero_when_is_negative(): void
@@ -53,6 +68,9 @@ class OrderItemTest extends TestCase
         $this->expectException(\Exception::class);
         $this->expectExceptionMessage('Quantity must be greater than zero');
 
+        // Arrange
+
+        // Act
         new OrderItem(
             id: '1',
             productId: 'p1',
@@ -60,6 +78,8 @@ class OrderItemTest extends TestCase
             quantity: -1,
             price: 100
         );
+
+        // Assert
     }
 
     public function test_should_throw_error_if_quantity_is_less_than_zero_when_is_zero(): void
@@ -67,6 +87,9 @@ class OrderItemTest extends TestCase
         $this->expectException(\Exception::class);
         $this->expectExceptionMessage('Quantity must be greater than zero');
 
+        // Arrange
+
+        // Act
         new OrderItem(
             id: '1',
             productId: 'p1',
@@ -74,6 +97,8 @@ class OrderItemTest extends TestCase
             quantity: 0,
             price: 100
         );
+
+        // Assert
     }
 
     public function test_should_throw_error_if_price_is_less_than_zero_when_is_negative(): void
@@ -81,6 +106,9 @@ class OrderItemTest extends TestCase
         $this->expectException(\Exception::class);
         $this->expectExceptionMessage('Price must be greater than zero');
 
+        // Arrange
+
+        // Act
         new OrderItem(
             id: '1',
             productId: 'p1',
@@ -88,6 +116,8 @@ class OrderItemTest extends TestCase
             quantity: 1,
             price: -1
         );
+
+        // Assert
     }
 
     public function test_should_throw_error_if_price_is_less_than_zero_when_is_zero(): void
@@ -95,6 +125,9 @@ class OrderItemTest extends TestCase
         $this->expectException(\Exception::class);
         $this->expectExceptionMessage('Price must be greater than zero');
 
+        // Arrange
+
+        // Act
         new OrderItem(
             id: '1',
             productId: 'p1',
@@ -102,5 +135,78 @@ class OrderItemTest extends TestCase
             quantity: 1,
             price: 0
         );
+
+        // Assert
+    }
+
+    public function test_should_create_new_order_item_successfully(): void
+    {
+        // Arrange
+        $orderItem = new OrderItem(
+            id: '1',
+            productId: 'p1',
+            name: 'Product 1',
+            quantity: 1,
+            price: 100
+        );
+
+        // Act
+
+        // Assert
+        $this->assertIsObject($orderItem);
+    }
+    
+    public function test_should_get_quantity_successfully(): void
+    {
+        // Arrange
+        $orderItem = new OrderItem(
+            id: '1',
+            productId: 'p1',
+            name: 'Product 1',
+            quantity: 1,
+            price: 100
+        );
+
+        // Act
+        $quantity = $orderItem->getQuantity();
+
+        // Assert
+        $this->assertEquals(1, $quantity);
+    }
+
+    public function test_should_get_price_successfully(): void
+    {
+        // Arrange
+        $orderItem = new OrderItem(
+            id: '1',
+            productId: 'p1',
+            name: 'Product 1',
+            quantity: 1,
+            price: 100
+        );
+
+        // Act
+        $price = $orderItem->getPrice();
+
+        // Assert
+        $this->assertEquals(100, $price);
+    }
+
+    public function test_should_get_total_price_successfully(): void
+    {
+        // Arrange
+        $orderItem = new OrderItem(
+            id: '1',
+            productId: 'p1',
+            name: 'Product 1',
+            quantity: 5,
+            price: 200
+        );
+
+        // Act
+        $totalPrice = $orderItem->getTotalPrice();
+
+        // Assert
+        $this->assertEquals(1000, $totalPrice);
     }
 }
