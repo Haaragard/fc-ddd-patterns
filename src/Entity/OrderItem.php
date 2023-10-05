9cf217a595a5f10a994a5041126710aa049a580d
Haaragard <haaragard@gmail.com>
2023-10-04 22:47:00 -0300

    feat: add order item validation

--- src/Entity/OrderItem.php
+++ src/Entity/OrderItem.php
@@ -6,10 +6,13 @@ class OrderItem
 {
     public function __construct(
         private string $id,
+        private string $productId,
         private string $name,
         private int $quantity,
         private int $price
-    ) {}
+    ) {
+        $this->validate();
+    }
 
     public function getQuantity(): int
     {
@@ -24,5 +27,28 @@ class OrderItem
     public function getTotalPrice(): int
     {
         return $this->getQuantity() * $this->getPrice();
+    }
+
+    private function validate(): void
+    {
+        if (empty($this->id)) {
+            throw new \Exception('Id is required');
+        }
+
+        if (empty($this->productId)) {
+            throw new \Exception('Product Id is required');
+        }
+
+        if (empty($this->name)) {
+            throw new \Exception('Name is required');
+        }
+
+        if ($this->quantity <= 0) {
+            throw new \Exception('Quantity must be greater than zero');
+        }
+
+        if ($this->price <= 0) {
+            throw new \Exception('Price must be greater than zero');
+        }
     }
 }
