8040b88bc733c950599d0689912e78ae507b27ee
Haaragard <haaragard@gmail.com>
2024-01-19 00:00:02 -0300

    add new order service place order method and tests

--- src/Service/OrderService.php
+++ src/Service/OrderService.php
@@ -2,7 +2,10 @@
 
 namespace App\Service;
 
+use App\Entity\Customer;
 use App\Entity\Order;
+use App\Entity\OrderItem;
+use Exception;
 
 class OrderService
 {
@@ -17,5 +20,23 @@ class OrderService
             fn(int $carry, Order $item) => $carry + $item->total(),
             0
         );
+    }
+
+    /**
+     * @param Customer $customer
+     * @param array<int, OrderItem> $items
+     * @return Order
+     * @throws Exception
+     */
+    public function placeOrder(Customer $customer, array $items): Order
+    {
+        if (empty($items)) {
+            throw new Exception('Order must have at least one item');
+        }
+
+        $order = new Order(uniqid('order-'), $customer->getId(), $items);
+        $customer->addRewardPoints($order->total()/2);
+
+        return $order;
     }
 }
--- tests/Unit/Service/OrderServiceTest.php
+++ tests/Unit/Service/OrderServiceTest.php
@@ -2,9 +2,11 @@
 
 namespace Tests\Unit\Service;
 
+use App\Entity\Customer;
 use App\Entity\Order;
 use App\Entity\OrderItem;
 use App\Service\OrderService;
+use Exception;
 use PHPUnit\Framework\TestCase;
 
 class OrderServiceTest extends TestCase
@@ -27,5 +29,26 @@ class OrderServiceTest extends TestCase
         $total = $this->orderService->total([$order1, $order2]);
 
         $this->assertEquals(500, $total);
+    }
+
+    public function test_should_place_an_order(): void
+    {
+        $customer = new Customer('c1', 'Customer 1');
+        $item1 = new OrderItem('i1', 'Item 1', 'Item 1', 1, 10);
+
+        $order = $this->orderService->placeOrder($customer, [$item1]);
+
+        $this->assertEquals(5, $customer->getRewardPoints());
+        $this->assertEquals(10, $order->total());
+    }
+
+    public function test_should_throw_an_error_when_place_an_order_receives_empty_order_items(): void
+    {
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage('Order must have at least one item');
+
+        $customer = new Customer('c1', 'Customer 1');
+
+        $this->orderService->placeOrder($customer, []);
     }
 }
