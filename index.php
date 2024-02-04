86f0bbcd043f15415b1b201b17f9a1912d1f65fe
Haaragard <haaragard@gmail.com>
2024-02-04 16:26:36 -0300

    feat: remove index code

--- index.php
+++ index.php
@@ -3,11 +3,5 @@
 require_once 'config/bootstrap.php';
 
 {
-    $product = new \App\Infrastructure\Database\Doctrine\Model\Product();
-    $product->setName('Product 1');
-    $product->setPrice(100);
-
-    $entityManager->persist($product);
-    $entityManager->flush();
 
 }
